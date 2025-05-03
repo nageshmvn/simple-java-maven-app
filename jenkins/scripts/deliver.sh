3e5f6612d791643b7a7969b54afb05cf3c8f0670
Nagesh Badiger <nagesh.badiger@gopoolit.com>
2025-05-03 21:12:26 +0530

    removed spcial charactoer from deliver file

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,37 +1,57 @@
-#!/usr/bin/env bash
+# #!/usr/bin/env bash
 
-echo 'The following Maven command installs your Maven-built Java application'
-echo 'into the local Maven repository, which will ultimately be stored in'
-echo 'Jenkins''s local Maven repository (and the "maven-repository" Docker data'
-echo 'volume).'
-set -x
-mvn jar:jar install:install help:evaluate -Dexpression=project.name
-set +x
+# echo 'The following Maven command installs your Maven-built Java application'
+# echo 'into the local Maven repository, which will ultimately be stored in'
+# echo 'Jenkins''s local Maven repository (and the "maven-repository" Docker data'
+# echo 'volume).'
+# set -x
+# mvn jar:jar install:install help:evaluate -Dexpression=project.name
+# set +x
+
+# echo 'The following command extracts the value of the <name/> element'
+# echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
+# set -x
+# NAME=`mvn -q -DforceStdout help:evaluate -Dexpression=project.name`
+# set +x
+
+# echo 'The following command behaves similarly to the previous one but'
+# echo 'extracts the value of the <version/> element within <project/> instead.'
+# set -x
+# VERSION=`mvn -q -DforceStdout help:evaluate -Dexpression=project.version`
+# set +x
+
+# # echo 'The following command runs and outputs the execution of your Java'
+# # echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
+# # set -x
+# # java -jar target/${NAME}-${VERSION}.jar
+
+# echo 'The following command runs and outputs the execution of your Java and Store in UUUU'
+# echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
+
+# # Remove all special characters from NAME and VERSION, keeping only alphanumerics, hyphens, and underscores
+# NAME=$(echo "$NAME" | tr -cd '[:alnum:]_-')
+# VERSION=$(echo "$VERSION" | tr -cd '[:alnum:]_-')
+
+# set -x
+# java -jar target/${NAME}-${VERSION}.jar
 
 echo 'The following command extracts the value of the <name/> element'
 echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
 set -x
-NAME=`mvn -q -DforceStdout help:evaluate -Dexpression=project.name`
+NAME=$(mvn -q -DforceStdout help:evaluate -Dexpression=project.name | sed 's/\x1b\[[0-9;]*m//g')
 set +x
 
 echo 'The following command behaves similarly to the previous one but'
 echo 'extracts the value of the <version/> element within <project/> instead.'
 set -x
-VERSION=`mvn -q -DforceStdout help:evaluate -Dexpression=project.version`
+VERSION=$(mvn -q -DforceStdout help:evaluate -Dexpression=project.version | sed 's/\x1b\[[0-9;]*m//g')
 set +x
 
-# echo 'The following command runs and outputs the execution of your Java'
-# echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
-# set -x
-# java -jar target/${NAME}-${VERSION}.jar
-
-echo 'The following command runs and outputs the execution of your Java and Store in UUUU'
-echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
-
-# Remove all special characters from NAME and VERSION, keeping only alphanumerics, hyphens, and underscores
+# Sanitize values
 NAME=$(echo "$NAME" | tr -cd '[:alnum:]_-')
-VERSION=$(echo "$VERSION" | tr -cd '[:alnum:]_-')
+VERSION=$(echo "$VERSION" | tr -cd '[:alnum:]_.-')
+
+echo "Resolved JAR name: target/${NAME}-${VERSION}.jar"
 
 set -x
 java -jar target/${NAME}-${VERSION}.jar
-
